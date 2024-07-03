92e1e73312f2b730eec557d90adbcb20142bd0cf
aleksey <tech.temerbek@gmail.com>
2024-07-03 18:36:11 +0300

    add hometask1

--- HomeTasks/GB_L2_HT1.py
+++ HomeTasks/GB_L2_HT1.py
@@ -3,3 +3,15 @@
 # монеток, которые нужно перевернуть, чтобы все монетки были
 # повернуты вверх одной и той же стороной. Выведите минимальное
 # количество монет, которые нужно перевернуть.
+
+import random
+
+n = int(input('Input coins quantity: '))
+
+coins = [random.randint(0,1) for i in range(n)]
+result = sum(coins)
+if result > n - result:
+    result = n - result
+
+print(coins)
+print(result)
